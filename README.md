632f2c6e51b895c78ec9ca22c76db0265697c87b
NingYanGuo <gny315@163.com>
2016-11-07 15:55:20 +0800

    remove test

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  - made for myself to finish small projects in company.
  - webpack在client目录下运行
  - 本地服务在dist目录下启动
-
+ - 全局安装karma
--- client/components/welcome/welcome.js
+++ client/components/welcome/welcome.js
@@ -1,14 +1,13 @@
-//document.write('This is welcome component');
-"use strict";
 import me from '../../asserts/images/me.jpg';
+"use strict";
 export default {
     data() {
         fetch('../components/welcome/welcome.json')
-    .then(function(response) {
-        return response.text()
-    }).then(function(newHTML) {
-        console.log(newHTML);
-    })
+        .then(function(response) {
+            return response.text()
+        }).then(function(newHTML) {
+            console.log(newHTML);
+        })
         document.write('<span>This is welcome component</span>');
         document.write('<img src="'+me+'">');
     }
